fd750d1ef4693e2ed5608c543d00c57e7da7cc9e
Amirhossein Asgharnia <asgharnia4@yahoo.com>
2025-09-11 11:58:10 -0400

    Change && to ||

--- PE/normalizer.m
+++ PE/normalizer.m
@@ -3,8 +3,13 @@ function critic = normalizer (critic)
 num_of_objectives = size(critic , 2);
 
 for i = 1 : num_of_objectives
+    
+    max_critic = max(critic(: , i));
+    min_critic = min(critic(: , i));
 
-    critic(: , i) = (critic(: , i) - min(critic(: , i))) /...
-        (max(critic(: , i)) - min(critic(: , i)));
+    if max_critic ~= 0 || min_critic ~= 0
+        critic(: , i) =...
+            (critic(: , i) - min_critic) / (max_critic - min_critic);
+    end
 
 end
